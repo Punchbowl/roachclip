a6dddad9a027973d127fe30ab4506b571b306144
Blake Carlson <blc@punchbowl.com>
2015-04-29 14:50:06 -0400

    Integrate MongoMapper into test suite.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,5 +1,21 @@
+require 'bundler/setup'
+Bundler.setup(:default, 'test', 'development')
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'roachclip'
 
+require 'mongo_mapper'
+require 'joint'
+
 require 'minitest/autorun'
 require 'minitest/pride'
+
+MongoMapper.database = "roachclip_test"
+
+class Minitest::Test
+
+  def setup
+    MongoMapper.database.collections.each { |coll| coll.remove unless coll.name =~ /^system/ }
+  end
+
+end
